967ca3abc4541f44e4f622c01099628926458940
Andre Lengwenus <alengwenus@gmail.com>
2020-05-16 23:34:18 +0200

    Refactored modules

---
+++ src/panels/config/lcn/show-dialog-scan-modules.ts
@@ -0,0 +1,26 @@
+import { fireEvent } from "../../../common/dom/fire_event";
+import { ScanModulesDialog } from "./lcn-scan-modules-dialog";
+
+const getDialog = () => {
+  return document
+    .querySelector("home-assistant")!
+    .shadowRoot!.querySelector("lcn-scan-modules-dialog") as
+    | ScanModulesDialog
+    | undefined;
+};
+
+export const loadLCNScanModulesDialog = () =>
+  import(
+    /* webpackChunkName: "lcn-scan-modules-dialog" */ "./lcn-scan-modules-dialog"
+  );
+
+export const showLCNScanModulesDialog = (
+  element: HTMLElement
+): (() => ScanModulesDialog | undefined) => {
+  fireEvent(element, "show-dialog", {
+    dialogTag: "lcn-scan-modules-dialog",
+    dialogImport: loadLCNScanModulesDialog,
+    dialogParams: {},
+  });
+  return getDialog;
+};
